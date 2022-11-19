d2f5094573d702570840a19080482fb39ae46c15
LauricioX <developerwebmasterpro@gmail.com>
2022-11-19 11:16:05 -0300

    ejs

---
+++ index.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, (erro) => {
+  if (erro) {
+    console.log("erro ao execultar!");
+  } else {
+    console.log("servidor online!");
+  }
+});
